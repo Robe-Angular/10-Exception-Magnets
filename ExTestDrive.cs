d2b3ee0f5a4ff9a21d16654aee7566b3b0a09fcd
robe <robe@robe.com>
2024-02-05 17:48:47 -0600

    Agregar archivos de proyecto.

---
+++ ExTestDrive.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Exception_Magnets
+{
+    class ExTestDrive
+    {
+        public static void Zero(string test)
+        {
+            Console.Write("t");
+            try
+            {
+                doRisky(test);
+                Console.Write("o");
+                
+            }
+            catch(MyException)
+            {
+                Console.Write("a");
+            }
+            finally
+            {
+
+                Console.Write("w");
+                Console.Write("s");
+            }
+        }
+
+        static void doRisky(string t)
+        {
+            Console.Write("h");
+            if ("yes".Equals(t))
+            {
+                
+                throw new MyException();
+            }
+            Console.Write("r");
+
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10_Exception_Magnets
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.Write("When it ");
+            ExTestDrive.Zero("yes");
+            Console.Write(" it ");
+            ExTestDrive.Zero("no");
+            Console.WriteLine(".");
+
+        }
+    }
+}
